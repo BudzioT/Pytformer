1a652e9fb2b12a4df2dcec099fd304c2fe6bfab1
BudzioT <Budzio.zsm1@gmail.com>
2024-07-01 12:21:04 +0200

    Drew the enemy, added sound files, created utilities file

---
+++ src/Utilities.py
@@ -0,0 +1,9 @@
+import os
+
+import pygame
+
+
+# Absolute path to current directory
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+# Images directory
+IMG_PATH = os.path.join(BASE_PATH, "dependencies/images")
